81b9fafb962f91655eb2162fd794c63fbb647e73
FernandoRafa2701 <rafaelnanoaramayo@gmail.com>
2023-07-05 11:44:14 -0300

    archivos

---
+++ AmigoDelUsuario.js
@@ -0,0 +1,9 @@
+/*Presentacion del Amigo del Usuario*/ 
+let miAmigo = {
+    nombre: "Steven",
+    altura: "1.80 m",
+    peso: "73.8 kg",
+    comidaFavorita: "Pizza ;)",
+};
+
+console.log("Hola Mundo, ¡eh aqui les presento a mi mejor amigo!, su nombre es : " + nombre + ", su altura es " + altura + ", y pesa: " + peso + " y su favorite food is: " + comidaFavorita);
---
+++ DatosUsuario.js
@@ -0,0 +1,17 @@
+/** Datos del Usuario*/
+
+let nombre = "Rafael";
+let apellido = "Usumaki";
+let edad = 35;
+let numeroDeTelefono = 3882356341;
+let fechaDeIngreso = 27/05/24;
+let precioDeLaFicha = 14000.50;
+console.log(nombre);
+
+console.log(apellido);
+console.log(edad);
+console.log(numeroDeTelefono);
+console.log(fechaDeIngreso);
+console.log(precioDeLaFicha);
+
+
